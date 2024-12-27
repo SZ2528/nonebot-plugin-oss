f558a6f9f148c09403c6d758fdd208a16045b917
SZ2528 <83620631+SZ2528@users.noreply.github.com>
2024-12-27 18:40:47 +0800

    🐛捉虫虫

--- nonebot_plugin_oss/__init__.py
+++ nonebot_plugin_oss/__init__.py
@@ -2,6 +2,7 @@ from nonebot import get_plugin_config
 from nonebot.plugin import PluginMetadata
 
 from .config import Config
+from . import seasonalbg
 
 __plugin_meta__ = PluginMetadata(
     name="osu!搜索歌曲",
