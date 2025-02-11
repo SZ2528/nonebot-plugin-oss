19b27e031bcb07135bacb3edc4aad7e35db18f40
Rain-0x01_ <83620631+SZ2528@users.noreply.github.com>
2025-02-11 18:42:21 +0800

    meow (#1)
    📐⚗️📐⚗️
    一言难尽啊。

--- nonebot_plugin_osu/get_auth.py
+++ nonebot_plugin_osu/get_auth.py
@@ -4,7 +4,7 @@ from nonebot import (
     require,
     get_plugin_config
 )
-from nonebot.adapters.onebot.v11 import Bot, MessageEvent
+from nonebot.adapters.onebot.v11 import Bot, MessageEvent, PrivateMessageEvent
 from nonebot.adapters.onebot.v11 import (
     Bot,
 )
@@ -16,14 +16,17 @@ from urllib.parse import urlencode
 import requests
 import json
 
-# 从nonebot配置读入client_id和client_secret
+# 从nonebot配置读入
 plugin_config = get_plugin_config(Config)
 client_id = plugin_config.osu_api_client_id
 client_secret = plugin_config.osu_api_client_secret
+safe_mode = plugin_config.osu_auth_safe_mode
 
 getauth_matcher = on_command("osugetauth", aliases={"osu获取授权", "获取osu授权"})
 @getauth_matcher.handle()
 async def _(bot: Bot, event: MessageEvent):
+    if safe_mode and not isinstance(event, PrivateMessageEvent):
+        await getauth_matcher.finish("请在私聊中使用")
     params = {
         'client_id': client_id,
         # 'redirect_uri': REDIRECT_URI,
@@ -36,6 +39,8 @@ async def _(bot: Bot, event: MessageEvent):
 gettoken_matcher = on_startswith("https://osu.ppy.sh/oauth/authorize", block = True)
 @gettoken_matcher.handle()
 async def _(bot: Bot, event: MessageEvent):
+    if safe_mode and not isinstance(event, PrivateMessageEvent):
+        await getauth_matcher.finish("请在私聊中使用")
     # 解析url中的code参数和state参数
     url = event.get_plaintext()
     code = url.split("code=")[1].split("&")[0]
@@ -66,5 +71,3 @@ async def _(bot: Bot, event: MessageEvent):
     else:
         await gettoken_matcher.finish(f"获取access token失败: {response.json()}")
         # raise Exception(f"Failed to get access token: {response.status_code}")
-
-    
